761db8085dbf61d3a0a94a449536e624f54e2a5a
NBlackout <NBlackout@hotmail.fr>
2013-07-12 20:26:03 +0200

    Sending activation email to complete registration works

--- app/controllers/UserController.java
+++ app/controllers/UserController.java
@@ -1,12 +1,29 @@
 package controllers;
 
 import models.User;
+import notifiers.Mail;
 
 import org.apache.commons.mail.EmailException;
 
 import play.libs.Crypto;
 
 public class UserController extends SuperController {
+
+	public static void activateUser(String activationToken) {
+		User user = User.find("byEmail", Crypto.decryptAES(activationToken)).first();
+
+		if (user != null) {
+			user.activated = Boolean.TRUE;
+			user.save();
+
+			/* Add user to session */
+			session.put("user.id", user.id);
+			session.put("user.username", user.username);
+			session.put("user.type", user.type);
+		}
+
+		Application.index();
+	}
 
 	public static void saveUser(String email, String username, String password) throws EmailException {
 		/* Parameters validation */
@@ -37,7 +54,7 @@ public class UserController extends SuperController {
 		user.save();
 
 		/* Activation mail sending */
-		MailController.sendActivationEmail(user);
+		Mail.sendActivationEmail(user);
 
 		Application.index();
 	}
---
+++ app/notifiers/Mail.java
@@ -0,0 +1,20 @@
+package notifiers;
+
+import models.User;
+import play.Play;
+import play.i18n.Messages;
+import play.libs.Crypto;
+import play.mvc.Mailer;
+
+public class Mail extends Mailer {
+
+	public static void sendActivationEmail(User user) {
+		setSubject(Messages.get("mail.activation.subject"));
+		setFrom(Play.configuration.getProperty("mail.from"));
+		addRecipient(user.email);
+
+		String userName = user.username;
+		String userEmail = Crypto.encryptAES(user.email);
+		send(userName, userEmail);
+	}
+}
